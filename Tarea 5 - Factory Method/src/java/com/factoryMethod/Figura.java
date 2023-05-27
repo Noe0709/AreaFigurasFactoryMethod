923e7601da293f905a89f155a3c2dc4348cb9068
Noe0709 <85904435+Noe0709@users.noreply.github.com>
2023-05-26 23:34:41 -0600

    Update Figura.java
    Agregando markdown

--- Tarea 5 - Factory Method/src/java/com/factoryMethod/Figura.java
+++ Tarea 5 - Factory Method/src/java/com/factoryMethod/Figura.java
@@ -5,12 +5,28 @@
 package com.factoryMethod;
 
 /**
- *
- * @author pablo
+ * La interfaz `Figura` define los métodos necesarios para calcular el perímetro y el área de una figura geométrica.
  */
 public interface Figura {
     
+    /**
+     * El método `perimetro` calcula el perímetro de la figura geométrica en base a los valores proporcionados.
+     * @param v1 Valor 1 necesario para el cálculo del perímetro.
+     * @param v2 Valor 2 necesario para el cálculo del perímetro.
+     * @param v3 Valor 3 necesario para el cálculo del perímetro.
+     * @param v4 Valor 4 necesario para el cálculo del perímetro.
+     * @return El perímetro de la figura geométrica.
+     */
     double perimetro(double v1, double v2, double v3, double v4);
+    
+    /**
+     * El método `area` calcula el área de la figura geométrica en base a los valores proporcionados.
+     * @param v1 Valor 1 necesario para el cálculo del área.
+     * @param v2 Valor 2 necesario para el cálculo del área.
+     * @param v3 Valor 3 necesario para el cálculo del área.
+     * @return El área de la figura geométrica.
+     */
     double area(double v1, double v2, double v3);
     
 }
+
