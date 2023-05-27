843efcb437b52e13929bc6f7085f448819eed72e
Noe0709 <85904435+Noe0709@users.noreply.github.com>
2023-05-26 23:32:29 -0600

    Update AreaFactory.java
    Comentado

--- Tarea 5 - Factory Method/src/java/com/factoryMethod/AreaFactory.java
+++ Tarea 5 - Factory Method/src/java/com/factoryMethod/AreaFactory.java
@@ -5,10 +5,17 @@
 package com.factoryMethod;
 
 /**
- *
- * @author pablo
+ * La clase `AreaFactory` es responsable de calcular el área de diferentes figuras geométricas utilizando el método Factory.
  */
 public class AreaFactory {
+        /**
+     * El método `obtenerArea` calcula el área de una figura geométrica específica en base a los valores proporcionados.
+     * @param v1 Valor 1 necesario para el cálculo del área.
+     * @param v2 Valor 2 necesario para el cálculo del área.
+     * @param v3 Valor 3 necesario para el cálculo del área.
+     * @param tipoFigura Tipo de figura geométrica para la cual se desea calcular el área.
+     * @return El área de la figura geométrica.
+     */
     
     public double obtenerArea(double v1, double v2, double v3, TipoFigura tipoFigura){
         double area = 0;
